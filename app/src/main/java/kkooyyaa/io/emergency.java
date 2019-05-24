b767b14590c4c8f23f286b8fcdec80178143c8a9
kkooyyaa <keu989.uregina@gmail.com>
2019-05-24 18:43:50 +0900

    add

--- app/src/main/java/kkooyyaa/io/emergency.java
+++ app/src/main/java/kkooyyaa/io/emergency.java
@@ -13,8 +13,6 @@ public class emergency extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
-
-
     }
 
     public void callManagement (View v) {
